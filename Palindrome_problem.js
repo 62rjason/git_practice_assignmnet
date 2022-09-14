35f31ffed259d2e801de452d1b03bff172301c5a
imransbr <imransbr7@gmail.com>
2022-09-14 15:41:18 +0530

    code has been fixed

--- Palindrome_problem.js
+++ Palindrome_problem.js
@@ -3,4 +3,8 @@ let rev="";
 for(let i=str.length;i>=0;i--){
 rev+=str[i];
 }
-if
+if(rev==str){
+    console.log("Palindrome")
+}else{
+    console.log("Not a Palindrom")
+}
--- Prime_problem.js
+++ Prime_problem.js
@@ -1,7 +1,11 @@
 let num=13;
-let f==0;
+let f=0;
 for(let i=1;i<=num;i++){
 if(num%i==0){
 f++;
 }}
 if(f==2){
+    console.log("Yes")
+}else{
+    console.log("No")
+}
